076a232e819f2fb1c594d15afbe5f54511000359
bitcko <m.backer.shehadi@gmail.com>
2018-07-04 21:56:37 +0300

    First Upload

---
+++ PayPalRestApi.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mhmdbackershehadi
+ * Date: 7/3/18
+ * Time: 11:08 PM
+ */
+namespace bitcko\paypalrestapi;
+
+use PayPal\Api\Amount;
+use PayPal\Api\Details;
+use PayPal\Api\Item;
+use PayPal\Api\ItemList;
+use PayPal\Api\Payer;
+use PayPal\Api\Payment;
+use PayPal\Api\RedirectUrls;
+use PayPal\Api\Transaction;
+use PayPal\Api\PaymentExecution;
+use PayPal\Exception\PayPalConnectionException;
+use Yii;
+class PayPalRestApi
+{
+    private $apiContext;
+    public $redirectUrl;
+    public $payPalClientId;
+    public $payPalClientSecret;
+
+    public function __construct()
+    {
+        $apiContext = new \PayPal\Rest\ApiContext(
+            new \PayPal\Auth\OAuthTokenCredential(
+                Yii::$app->params['payPalClientId'],
+                Yii::$app->params['payPalClientSecret']
+            )
+        );
+        $this->apiContext = $apiContext;
+    }
+
+
+    public function checkOut($params){
+
+        $payer = new Payer();
+        $payer->setPaymentMethod($params['method']);
+        $orderList = [];
+        foreach ($params['order']['items'] as $orderItem){
+            $item = new Item();
+            $item->setName($orderItem['name'])
+                ->setCurrency($orderItem['currency'])
+                ->setQuantity($orderItem['quantity'])
+                ->setPrice($orderItem['price']);
+            $orderList[]=$item;
+        }
+        $itemList = new ItemList();
+        $itemList->setItems($orderList);
+
+        $details = new Details();
+        $details->setShipping($params['order']['shippingCost'])
+
+            ->setSubtotal($params['order']['subtotal']);
+        $amount = new Amount();
+        $amount->setCurrency($params['order']['currency'])
+            ->setTotal($params['order']['total'])
+            ->setDetails($details);
+
+        $transaction = new Transaction();
+        $transaction->setAmount($amount)
+            ->setItemList($itemList)
+            ->setDescription($params['order']['description'])
+            ->setInvoiceNumber(uniqid());
+
+        $redirectUrl = Url::to([$this->redirectUrl],true);
+
+
+        $redirectUrls = new RedirectUrls();
+        $redirectUrls->setReturnUrl("$redirectUrl?success=true")
+            ->setCancelUrl("$redirectUrl?success=false");
+
+        $payment = new Payment();
+        $payment->setIntent($params['intent'])
+            ->setPayer($payer)
+            ->setRedirectUrls($redirectUrls)
+            ->setTransactions(array($transaction));
+
+
+        try {
+            $payment->create($this->apiContext);
+            return \Yii::$app->controller->redirect($payment->getApprovalLink());
+        }
+        catch (PayPalConnectionException $ex) {
+            // This will print the detailed information on the exception.
+            //REALLY HELPFUL FOR DEBUGGING
+            \Yii::$app->response->format = \yii\web\Response::FORMAT_HTML;
+            \Yii::$app->response->data = $ex->getData();
+        }
+
+    }
+
+    public function processPayment($params){
+
+        if (isset(Yii::$app->request->get()['success']) && Yii::$app->request->get()['success'] == 'true') {
+            $paymentId = Yii::$app->request->get()['paymentId'];
+            $payment = Payment::get($paymentId, $this->apiContext);
+            $execution = new PaymentExecution();
+            $execution->setPayerId(Yii::$app->request->get()['PayerID']);
+
+            $transaction = new Transaction();
+
+            $amount = new Amount();
+
+            $details = new Details();
+            $details->setShipping($params['order']['shippingCost'])
+                ->setSubtotal($params['order']['subtotal']);
+            $amount->setCurrency($params['order']['currency']);
+            $amount->setTotal($params['order']['total']);
+            $amount->setDetails($details);
+
+            $transaction->setAmount($amount);
+            $execution->addTransaction($transaction);
+
+            try {
+                $payment->execute($execution, $this->apiContext);
+
+                try {
+                    $payment = Payment::get($paymentId, $this->apiContext);
+                } catch (\Exception $ex) {
+                    \Yii::$app->response->format = \yii\web\Response::FORMAT_HTML;
+                    \Yii::$app->response->data = $ex->getData();
+
+                }
+            } catch (\Exception $ex) {
+                \Yii::$app->response->format = \yii\web\Response::FORMAT_HTML;
+                \Yii::$app->response->data = $ex->getData();
+
+            }
+
+
+            \Yii::$app->response->data =  $payment;
+
+        }
+
+        return Null;
+
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,133 @@
-# yii2-bitcko-paypal-api
+Yii2 Bitcko PayPal PayPal Api Extension
+=========================================
+Yii2 Bitcko PayPal PayPal Api extension  use to integrate simple PayPal payment in your website.
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require bitcko/yii2-bitcko-paypal-api:dev-master
+
+```
+
+or add
+
+```
+"bitcko/bitcko/yii2-bitcko-paypal-api": "dev-master"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+1. Create developer account in PayPal and then create an app.  [PayPal Developer Dashboard](https://developer.paypal.com/).
+2. Copy and paste the client Id and client secret in params.php file that exists in config directory:
+```php
+<?php
+
+return [
+    'adminEmail' => 'admin@example.com',
+    'payPalClientId'=>'app client id here',
+    'payPalClientSecret'=>'app client secret here'
+];
+
+
+```
+3. Setup the extension configuration in config/web.php file for yii2 basic temp or config/main.php file for Yii2 advanced temp in components section: 
+
+```php
+<?php
+'components'=> [
+    ...
+ 'PayPalRestApi'=>[
+            'class'=>'bitcko\paypalrestapi\PayPalRestApi',
+            'redirectUrl'=>'/site/make-payment', // Redirect Url after payment
+            ]
+            ...
+        ]
+
+```
+4. Controller example:
+
+```php
+<?php
+
+namespace app\controllers;
+
+use Yii;
+
+use yii\web\Controller;
+
+class PayPalApiController extends Controller
+{
+   
+    public function actionCheckout(){
+        // Setup order information array with all items
+        $params = [
+            'method'=>'paypal',
+            'intent'=>'sale',
+            'order'=>[
+                'description'=>'Payment description',
+                'subtotal'=>44,
+                'shippingCost'=>0,
+                'total'=>44,
+                'currency'=>'USD',
+                'items'=>[
+                    [
+                        'name'=>'Item one',
+                        'price'=>10,
+                        'quantity'=>1,
+                        'currency'=>'USD'
+                    ],
+                    [
+                        'name'=>'Item two',
+                        'price'=>12,
+                        'quantity'=>2,
+                        'currency'=>'USD'
+                    ],
+                    [
+                        'name'=>'Item three',
+                        'price'=>1,
+                        'quantity'=>10,
+                        'currency'=>'USD'
+                    ],
+
+                ]
+
+            ]
+        ];
+
+        Yii::$app->PayPalRestApi->checkOut($params);
+    }
+
+    public function actionMakePayment(){
+         // Setup order information array 
+        $params = [
+            'order'=>[
+                'description'=>'Payment description',
+                'subtotal'=>44,
+                'shippingCost'=>0,
+                'total'=>44,
+                'currency'=>'USD',
+            ]
+        ];
+      // In case of payment success this will return the payment object that contains all information about the payment
+      // In case of failure it will return Null
+      return  Yii::$app->PayPalRestApi->processPayment($params);
+
+    }
+}
+
+
+```
+
+
+
